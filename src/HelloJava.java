523a49d97299a1920969a80c268c24ad92997998
devpigKing <devpigking@gmail.com>
2023-11-29 01:03:19 +0900

    HelloJava 자바 복습 환영식

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class HelloJava {
-}
+public class HelloJava {
+
+    // 자바는 main(String[] args) 메서드를 찾아서 프로그램을 시작한다.
+    public static void main(String[] args) {
+        // 자바 언어는 대소문자를 구분한다. 대소문자가 다르면 오류가 발생할 수 있다.
+
+        // 값을 콘솔에 출력하는 기능이다. 자바는 문자열을 사용할 때 쌍따옴표를 사용한다.
+        System.out.println("hello java");
+
+    } // main() 메서드의 끝
+} // HelloJava 클래스의 끝
