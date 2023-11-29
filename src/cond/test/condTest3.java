8ae6c713401d6eaeb9f4044d2d499b146ac2f3bb
devpigKing <devpigking@gmail.com>
2023-11-30 01:58:08 +0900

    환율 계산하기

---
+++ src/cond/test/condTest3.java
@@ -0,0 +1,17 @@
+package cond.test;
+
+public class condTest3 {
+    public static void main(String[] args) {
+        int dollar = 5;
+
+        int won = dollar * 1300;
+
+        if (dollar < 0) {
+            System.out.println("잘못된 금액입니다.");
+        } else if (dollar == 0) {
+            System.out.println("환전할 금액이 없습니다.");
+        } else if (dollar > 0) {
+            System.out.println("출력 : 환전 금액은 " + won + "원입니다.");
+        }
+    }
+}
