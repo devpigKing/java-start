601e40b95777636a06e66f683bad234af111e436
devpigKing <devpigking@gmail.com>
2023-11-29 22:11:29 +0900

    주석이란?

---
+++ src/CommentJava.java
@@ -0,0 +1,16 @@
+public class CommentJava {
+
+    // 주석
+    // 주석은 자바 프로그램이 읽지 않고 무시하는 부분이다.
+    // 사람이 읽기 위해서 사용한다.
+    public static void main(String[] args) {
+        System.out.println("hello java1"); // hello java1을 출력합니다.
+        //System.out.println("hello java2");
+
+        /* 여러줄 주석
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+        */
+
+    }
+}
