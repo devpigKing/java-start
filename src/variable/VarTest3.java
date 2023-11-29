32e12ea7f89376abc4daf0745b9d4f0ba9dc66b7
devpigKing <devpigking@gmail.com>
2023-11-29 23:11:54 +0900

    변수 파트 테스트 3(long, Boolean)

---
+++ src/variable/VarTest3.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class VarTest3 {
+    public static void main(String[] args) {
+        long L = 10000000000L;
+        System.out.println(L);
+
+        boolean b = true;
+        System.out.println(b);
+    }
+}
