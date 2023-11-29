95c7e9845552b40ebf0e0c0a8f801b0d17eae587
devpigKing <devpigking@gmail.com>
2023-11-29 22:15:28 +0900

    변수 시작 선언 및 초기화

---
+++ src/variable/Var2.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Var2 {
+    public static void main(String[] args) {
+        int a = 10; // 변수 선언 및 초기화
+
+        System.out.println(a);
+
+        a = 15;
+        System.out.println(a);
+
+    }
+}
