1d59481b0308d275be9cf86bf4af1326d0afaf62
devpigKing <devpigking@gmail.com>
2023-11-30 00:42:46 +0900

    연산자 Test1

---
+++ src/operator/test/OperationTest1.java
@@ -0,0 +1,14 @@
+package operator.test;
+
+public class OperationTest1 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+        int num3 = 30;
+        int sum = num1 + num2 + num3;
+        double average = sum/3;
+
+        System.out.println(sum);
+        System.out.println(average);
+    }
+}
