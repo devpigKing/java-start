7140fccbf942ccdc53ef53803187d02be8584ab9
devpigKing <devpigking@gmail.com>
2023-11-29 22:22:04 +0900

    변수 변경

---
+++ src/variable/Var3.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Var3 {
+    public static void main(String[] args) {
+
+        int a = 10; // 변수 선언 및 초기화
+        System.out.println(a);
+
+        a = 77; // 변수 값 변경
+        System.out.println(a);
+
+    }
+}
