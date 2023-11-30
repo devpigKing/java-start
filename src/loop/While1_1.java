a1e717b3c63baed249ebac55ab93dcdca95b4fe1
devpigKing <devpigking@gmail.com>
2023-11-30 17:04:04 +0900

    반복문 이론, 예제

---
+++ src/loop/While1_1.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class While1_1 {
+    public static void main(String[] args) {
+        int cnt = 0;
+
+        cnt = cnt + 1;
+        System.out.println(cnt);
+        cnt = cnt + 1;
+        System.out.println(cnt);
+        cnt = cnt + 1;
+        System.out.println(cnt);
+
+    }
+}
